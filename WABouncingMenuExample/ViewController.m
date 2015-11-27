c8bcc313b4bd9ac9175d67292d58e2d410f20a98
Wendy Abrantes <abranteswendy@gmail.com>
2015-11-27 08:04:10 +0000

    remove touch begin

--- WABouncingMenuExample/ViewController.m
+++ WABouncingMenuExample/ViewController.m
@@ -47,14 +47,5 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    [super touchesBegan:touches withEvent:event];
-    if(!bcView.isOpen){
-        [bcView animateOpen];
-    }else{
-        [bcView animateClose];
-    }
-}
 
 @end
